413fce62f57f759d135d290b1b8c9a2af682b186
Sudip-Chel <118715456+Sudip-Chel@users.noreply.github.com>
2026-09-06 01:59:11 +0530

    Add files via upload

---
+++ Day_4/Task_Banking/playwright.config.ts
@@ -0,0 +1,48 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+
+    testDir: './tests',
+
+    globalSetup: require.resolve('./global-setup'),
+
+    globalTeardown: require.resolve('./global-teardown'),
+
+    timeout: 30000,
+
+    expect: {
+        timeout: 5000
+    },
+
+    fullyParallel: false,
+
+    reporter: [
+        ['html'],
+        ['list'],
+        ['allure-playwright']
+    ],
+
+    use: {
+        // baseURL: 'https://www.playwrightpad.in',
+
+        storageState: './auth/auth.json',
+
+        trace: 'on-first-retry',
+
+        screenshot: 'only-on-failure',
+
+        video: 'retain-on-failure',
+
+        headless: true
+    },
+
+    projects: [
+        {
+            name: 'chromium',
+            use: {
+                ...devices['Desktop Chrome']
+            }
+        },
+
+    ]
+});
